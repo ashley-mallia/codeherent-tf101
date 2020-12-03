a2bd8573d06a6084e0e1722f32d47958be81a47a
HashiCorp Training <training@hashicorp.com>
2020-12-03 03:42:21 +0000

    initial commit Terraform 101

---
+++ server/server.tf
@@ -0,0 +1,49 @@
+
+variable ami {}
+variable subnet_id {}
+variable vpc_security_group_ids {
+  type = list
+}
+variable identity {}
+variable key_name {}
+variable private_key {}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name               = var.key_name
+
+  tags = {
+    "Identity"    = var.identity
+    "Name"        = "Student"
+    "Environment" = "Training"
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = var.private_key
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "assets"
+    destination = "/tmp/"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.web.public_dns
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+
+variable access_key {}
+variable secret_key {}
+variable ami {}
+variable subnet_id {}
+variable identity {}
+variable region {}
+variable vpc_security_group_ids {
+  type = list
+}
+
